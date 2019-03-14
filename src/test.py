95dc80ee8483ed5a87b48dbe1e10d2e1ad623ced
campovski <campovski@gmail.com>
2019-03-14 10:06:56 +0100

    Add test.py

---
+++ src/test.py
@@ -0,0 +1,28 @@
+import serial
+import sys
+
+
+if __name__ =="__main__":
+    ser = None
+    try:
+        ser = serial.Serial(
+            port="COM16",
+            baudrate=2400,
+            bytesize=serial.SEVENBITS,
+            parity=serial.PARITY_EVEN,
+            stopbits=serial.STOPBITS_ONE,
+            xonxoff=True
+        )
+    except ValueError:
+        print("Bad parameters in serial.Serial")
+        sys.exit(1)
+    except serial.SerialException:
+        print("Cannot find device on given port")
+        sys.exit(2)
+
+    str_write = "I4\r\n"
+    ser.write(str_write.encode("ascii"))
+    str_read = ser.read_until(expected=serial.CRLF)
+    if is_instance(str_read, bytes):
+        str_read = str_read.decode("ascii")
+    print(str_read)
